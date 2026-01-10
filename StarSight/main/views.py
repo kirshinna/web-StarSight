736fca3e52dd7f83704d8fcd48af4764d76d0dd5
kirshinna <kirshina_nastya.2006@mail.ru>
2026-01-10 21:52:47 +0500

    Implemented a simple API call

--- StarSight/main/views.py
+++ StarSight/main/views.py
@@ -1,15 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
+
+API_KEY = '12c340b2c8e9d94fdb3c80b1c9daebd4'
 
 def index(request):
     city = request.GET.get('city')
     result = None
 
     if city:
-        result = f"Идёт проверка условий для города {city}..."
+        result = fetch_weather_data(city, API_KEY)
+
+    else:
+        result = "Не удалось получить данные о погоде."
 
     data = {
         'result': result
     }
 
     return render(request, 'main/index.html', data)
+
+
+def fetch_weather_data(city, api_key):
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        print("Ошибка запроса:", response.status_code, response.text)
+        return None
+
+    data = response.json()
+
+    return data
