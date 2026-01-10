6424292cb8d56bbfbdd2fa4c99cb935fff9c9b9f
kirshinna <kirshina_nastya.2006@mail.ru>
2026-01-10 23:04:52 +0500

    Change API, implement data output in a few days

--- StarSight/main/views.py
+++ StarSight/main/views.py
@@ -2,33 +2,47 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import requests
 
-API_KEY = '12c340b2c8e9d94fdb3c80b1c9daebd4'
-
+API_KEY = 'VBPG6AVYZLFFBYEQVKJYWUUXM'
 def index(request):
+    return render(request, 'main/index.html')
+
+def weather_check(request):
     city = request.GET.get('city')
+    start = request.GET.get('start')
+    end = request.GET.get('end')
+
     result = None
+    days = None
 
-    if city:
-        result = fetch_weather_data(city, API_KEY)
+    if city and start and end:
+        weather = fetch_weather_data(city,API_KEY ,start, end)
 
-    else:
-        result = "Не удалось получить данные о погоде."
+        if weather:
+            days = weather["days"]
+            result = f"Нашёл {len(days)} дней прогноза"
+        else:
+            result = "Не удалось получить данные."
 
-    data = {
-        'result': result
+    return render(request, 'main/index.html', {
+        'result': result,
+        'days': days
+    })
+
+
+def fetch_weather_data(city, api_key, start, end):
+    url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{city}/{start}/{end}"
+
+    params = {
+        "unitGroup": "metric",
+        "include": "days",
+        "key": api_key,
+        "contentType": "json"
     }
-
-    return render(request, 'main/index.html', data)
-
-
-def fetch_weather_data(city, api_key):
-    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
-    response = requests.get(url)
+    response = requests.get(url, params=params)
 
     if response.status_code != 200:
         print("Ошибка запроса:", response.status_code, response.text)
         return None
 
-    data = response.json()
+    return response.json()
 
-    return data
