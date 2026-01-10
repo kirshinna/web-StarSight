117b9fb991f6aa5d81ab869312aadca4c2d684de
kirshinna <kirshina_nastya.2006@mail.ru>
2026-01-10 20:54:00 +0500

    Create the initial page template

--- StarSight/StarSight/settings.py
+++ StarSight/StarSight/settings.py
@@ -18,6 +18,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-zqkasd6updibfw5u%(-6#4y*_b338a7om*&$gixslc+hl1-%i1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -113,3 +115,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_DIRS = [
+    BASE_DIR / "static"
+]
--- StarSight/main/views.py
+++ StarSight/main/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    city = request.GET.get('city')
+    result = None
+
+    if city:
+        result = f"Идёт проверка условий для города {city}..."
+
+    data = {
+        'result': result
+    }
+
+    return render(request, 'main/index.html', data)
